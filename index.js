6dfdb281eb95b8a8815dc2028e2d081f535285a1
WilliamCaesar <williamcaesar@pm.me>
2020-05-01 10:55:41 -0300

    config develop branch

--- index.js
+++ index.js
@@ -2,16 +2,14 @@ const Discord = require('discord.js');
 
 const bot = new Discord.Client();
 
-const token = 'NzA1NjI3MTY0Njk0ODA2NTgx.XqugHw.SDKal62BVQmEVLhFz_js7K5FVqo';
+bot.login('NzA1NjI3MTY0Njk0ODA2NTgx.XqugHw.SDKal62BVQmEVLhFz_js7K5FVqo');
 
-bot.login(token);
-
-bot.on('ready', ()=> {
+bot.on('ready', () => {
     console.log('entrei carai');
 });
 
-bot.on('message' , msg=>{
-    if (msg.content==='Salve') {
+bot.on('message', msg => {
+    if (msg.content === 'Salve') {
         msg.reply('fala tu');
     }
 });
